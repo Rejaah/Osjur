c53425e921466484830a43e52f1570fb6e4e9417
KarRz <reza11781@gmail.com>
2024-08-08 10:54:24 +0700

    add Navbar

--- portofolio/app/page.tsx
+++ portofolio/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-black">
-      <div className="container mx-auto px-12 py-4">
+      <Navbar/>
+      <div className="container mx-auto px-12 py-4 mt-24">
         <HeroSection/>
       </div>
     </main>
