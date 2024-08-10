ea051bac1aa8d602f1fe0b58906fb8b1f3b9415b
KarRz <reza11781@gmail.com>
2024-08-10 21:56:58 +0700

    add ProjectSection

--- portofolio/app/components/ProjectCard.jsx
+++ portofolio/app/components/ProjectCard.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Link from "next/link";
 
-const ProjectCard = ({ imgUrl, title, description }) => {
+const ProjectCard = ({ imgUrl, title, description, url }) => {
   return (
     <div>
         <div 
-            className="h-52 md:h-72" 
+            className="h-52 md:h-72 rounded-t-xl relative group" 
             style={{ background: `url(${imgUrl})`, backgroundSize: "cover"}}>
+            <Link href={url}>
+              <div className='overlay absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500'>
+              </div>
+            </Link>
         </div>
-        <div>
-            <h2>{title}</h2>
-            <p>{description}</p>
+        <div className='text-white rounded-b-xl bg-[#181818] py-6 px-4'>
+            <h2 className='text-xl font-semibold mb-2'>{title}</h2>
+            <p  className='text-[#ADB7BE] '>{description}</p>
         </div>
     </div>
   )
--- portofolio/app/components/ProjectSection.jsx
+++ portofolio/app/components/ProjectSection.jsx
@@ -1,15 +1,40 @@
 import React from 'react'
 import ProjectCard from './ProjectCard'
 
+const projectData = [
+  {
+    id: 1,
+    title: "Dukun Modern",
+    description: "GimJam Projects",
+    image:"/images/projects/DukunModern.png",
+    tag: ["All", "Game"],
+    url: "https://lotsadelight.itch.io/dukun-modern",
+  },
+  {
+    id: 2,
+    title: "Lintas Sumatra",
+    description: "Gim Projects",
+    image:"/images/projects/LintasSumatra.png",
+    tag: ["All", "Game"],
+    url:"https://lotsadelight.itch.io/lintas-sumatera",
+  },
+];
 
 const ProjectSection = () => {
   return (
-    <>
-        <h2>My Projects</h2>
-        <div>
-
+    <section id="projects" className='py-4 px-4 xl:gap-8 sm:py-8 xl:px-16'>
+        <h2 className='text-center text-4xl font-bold text-white mt-4 mb-10 px-4'>My Projects</h2>
+        <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
+          {projectData.map((project) => 
+          <ProjectCard 
+            key={project.id} 
+            title={project.title} 
+            description={project.description} 
+            imgUrl={project.image}
+            url={project.url}
+          />)}
         </div>
-    </>
+    </section>
   )
 }
 
